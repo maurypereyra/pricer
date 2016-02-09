d73ddba9a60e1b03a496265eadee44b9e68a06c6
Mauricio Pereyra <maury.pereyra@gmail.com>
2016-02-09 19:42:51 -0300

    Mauricio Pereyra - [AUTOMATION] - Adding cucumber plugin. It has an error with the webdriver.

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -30,17 +30,17 @@ grails.project.dependency.resolution = {
         //mavenRepo "http://repository.jboss.com/maven2/"
     }
     def gebVersion = "0.9.2"
-    def seleniumVersion = "2.45.0"
+    def seleniumVersion = "2.22.0"
     dependencies {
         // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
         test "org.gebish:geb-junit4:$gebVersion"
 
         test "org.seleniumhq.selenium:selenium-chrome-driver:$seleniumVersion"
         test "org.seleniumhq.selenium:selenium-support:$seleniumVersion"
-        test "org.seleniumhq.selenium:selenium-firefox-driver:$seleniumVersion"
+        /*test "org.seleniumhq.selenium:selenium-firefox-driver:$seleniumVersion"
         test("org.seleniumhq.selenium:selenium-htmlunit-driver:$seleniumVersion") {
             exclude 'xml-apis'
-        }
+        }*/
     }
 
     plugins {
@@ -49,6 +49,7 @@ grails.project.dependency.resolution = {
         runtime ":resources:1.1.6"
 
         test ":geb:$gebVersion"
+        test ":cucumber:0.5.0"
         //test ":spock:0.7" //If I want to use spock to do the test
 
         // Uncomment these (or add new ones) to enable additional resources capabilities
