8a6193a47358ae03399e6dd23721578c8fb74da6
Mauricio Pereyra <maury.pereyra@gmail.com>
2016-02-15 01:02:17 -0300

    Mauricio Pereyra - Updating Store Unit Test and  constrains

--- grails-app/domain/theprizypricer/Store.groovy
+++ grails-app/domain/theprizypricer/Store.groovy
@@ -7,7 +7,13 @@ class Store {
     static hasMany = [prices:Price]
 
     static constraints = {
-        name(nullable: false)
+        name blank: false, nullable: false
+        address blank: true, nullable: true
     }
 
+    static mapping = {
+        version false
+        name nullable: false
+        address nullable: true
+    }
 }
--- test/unit/theprizypricer/StoreTests.groovy
+++ test/unit/theprizypricer/StoreTests.groovy
@@ -2,14 +2,36 @@ package theprizypricer
 
 import grails.test.mixin.TestMixin
 import grails.test.mixin.support.GrailsUnitTestMixin
+import org.junit.Before
 
 /**
  * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
  */
 @TestMixin(GrailsUnitTestMixin)
 class StoreTests {
+    def Store store
 
-    void testSomething() {
-       fail "Implement me"
+    @Before
+    void setUp() {
+        store = new Store(name: "Walmart", address: "USA")
+    }
+
+    void testValidateNullableFields() {
+        mockForConstraintsTests(Store)
+        store = new Store()
+        assertFalse store.validate()
+        assertNull store.errors['address']
+        assertEquals 'nullable', store.errors['name']
+        assertEquals 1, store.errors.errorCount
+    }
+
+    void testValidateEmptyConstraints(){
+        mockForConstraintsTests(Store)
+        store.name = ""
+        store.address = ""
+        assertFalse store.validate()
+        assertNull store.errors['address']
+        assertEquals 'blank', store.errors['name']
+        assertEquals 1, store.errors.errorCount
     }
 }
